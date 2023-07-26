51d9e4aedb2d660b12971b8a6677b0926b33afcd
Ahmed Ibrahim <Ahmed.Ahmed.Ibrahim2003@gmail.com>
2023-07-26 03:23:40 +0300

    add book detail text and rate

--- lib/features/home/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/view/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/features/home/presentation/view/widgets/item_rate.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
@@ -12,14 +13,40 @@ class BookDetailsViewBody extends StatelessWidget {
     return SafeArea(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 3.w),
-        child: const Column(
+        child: Column(
           children: [
-            BookDetailsCustomAppBar(),
-            CustomBookImageBox(),
+            const BookDetailsCustomAppBar(),
+            const Padding(
+              padding: EdgeInsets.only(top: 30),
+              child: CustomBookImageBox(),
+            ),
+            const SizedBox(
+              height: 45,
+            ),
+            Text(
+              'The Jungle Book',
+              style: TextStyle(
+                fontFamily: 'GTSectraFine',
+                fontSize: 27.sp,
+              ),
+            ),
+            Text(
+              'Rudyard Kipling',
+              style: TextStyle(
+                fontSize: 13.sp,
+                color: Colors.white.withOpacity(0.7),
+              ),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            SizedBox(
+              width: 27.w,
+              child: const ItemRate(),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
