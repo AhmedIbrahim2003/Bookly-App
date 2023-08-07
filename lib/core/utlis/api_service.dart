86c4c7c8f62a72295f4a92cec1ee5b55d8f4d3a6
Ahmed Ibrahim <Ahmed.Ahmed.Ibrahim2003@gmail.com>
2023-08-08 01:57:04 +0300

    create api service

---
+++ lib/core/utlis/api_service.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: unused_field
+
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final _baseUrl = 'https://www.googleapis.com/books/v1/';
+  final Dio dio;
+
+  ApiService(this.dio);
+  Future<Map<String, dynamic>> get({required endPoint}) async {
+    var response = await dio.get('$_baseUrl$endPoint');
+    return response.data;
+  }
+}
