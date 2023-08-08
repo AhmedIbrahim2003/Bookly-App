c23f9380bafe808e16370636ebdc48df79c93e56
Ahmed Ibrahim <Ahmed.Ahmed.Ibrahim2003@gmail.com>
2023-08-08 23:26:31 +0300

    finished data layer

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,1 +1,3 @@
-abstract class Failuer {}
+abstract class Failuer{}
+
+class ServerFaliuer extends Failuer{}
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,18 +1,48 @@
 import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/core/utlis/api_service.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
-class HomeRepoImpl implements HomeRepo{
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failuer, List<BookModel>>> fetchBestSellerBook() {
-    // TODO: implement fetchBestSellerBook
-    throw UnimplementedError();
+  Future<Either<Failuer, List<BookModel>>> fetchNewestBook() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:programming');
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      // ignore: avoid_print
+      print('$e');
+      return left(ServerFaliuer());
+    }
   }
 
   @override
-  Future<Either<Failuer, List<BookModel>>> fetchFeaturedBook() {
-    // TODO: implement fetchFeaturedBook
-    throw UnimplementedError();
+  Future<Either<Failuer, List<BookModel>>> fetchFeaturedBook() async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      // ignore: avoid_print
+      print('$e');
+      return left(ServerFaliuer());
+    }
   }
 }
